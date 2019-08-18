5a3c4719fa15883c8d9171558d7f70e7c072a6e4
pradeep-charism <pradeep.charism@gmail.com>
2019-08-19 01:36:51 +0800

    Create ABCoin contract

--- src/js/abcoin.js
+++ src/js/abcoin.js
@@ -42,6 +42,7 @@ App = {
   bindEvents: function () {
     $(document).on('click', '.btn-show-balance', App.showTotalSupply);
     $(document).on('click', '.btn-issue-token', App.issueTokens);
+    $(document).on('click', '.btn-transfer', App.transferTokens);
   },
 
   loadOnStartup: function (event) {
@@ -93,6 +94,30 @@ App = {
         App.contracts.ABCoinContract.deployed().then(function (instance) {
           abcoinInstance = instance;
           return abcoinInstance.issueTokens({ from: account });
+        }).then(function (result) {
+          console.log("issueTokens", `${result}`);
+          return App.loadOnStartup();
+        }).catch(function (err) {
+          console.log(err.message);
+        });
+      });
+    },
+
+    transferTokens: function (event) {
+    event.preventDefault();
+
+      var abcoinInstance;
+      web3.eth.getAccounts(function (error, accounts) {
+        if (error) {
+          console.log(error);
+        }
+        var fromAccount = accounts[0];
+        var toAccount = accounts[1];
+        console.log("From account: ", fromAccount);
+        console.log("To account: ", toAccount);
+        App.contracts.ABCoinContract.deployed().then(function (instance) {
+          abcoinInstance = instance;
+          return abcoinInstance.transfer(toAccount, 1000, { from: fromAccount });
         }).then(function (result) {
           console.log("issueTokens", `${result}`);
           return App.loadOnStartup();
